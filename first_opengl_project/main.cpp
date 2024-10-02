13e17e3b8b0f1970e11623c30db57069b12b15d2
ckremer99 <ckremer99@outlook.com>
2024-10-01 17:57:00 -0700

    Initial Commit

---
+++ first_opengl_project/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  first_opengl_project
+//
+//  Created by Cody Kremer on 10/1/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
